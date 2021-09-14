73acdc34de37248e718ac4b1d2eef3bd93fa8ddc
taka <taka@mewpot.com>
2021-09-14 11:44:08 +0900

    [Study] Modify, Update, Delete 추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,38 @@
 class HomeController < ApplicationController
 
   def index
-
+    @posts = Post.all
   end
 
   def write
   
+  end
+
+  def create #액션
+    post = Post.new
+    post.title = params[:title]
+    post.content = params[:content]
+    post.save
+
+    redirect_to '/'
+  end
+
+  def modify
+    @post = Post.find(params[:post_id])
+  end
+
+  def update
+    post = Post.find(params[:post_id])
+    post.title = params[:title]
+    post.content = params[:content]
+    post.save
+
+    redirect_to '/'
+  end
+
+  def delete
+    Post.destroy(params[:post_id])
+    redirect_to '/'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   get '/' => 'home#index'
   get '/write' => 'home#write'
+  post '/create' => 'home#create'
+
+  get '/modify/:post_id' => 'home#modify'
+  post '/update/:post_id' => 'home#update'
+  get '/delete/:post_id' => 'home#delete'
 end
